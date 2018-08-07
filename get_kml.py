13fb48d134e28f01b067c636e7f211b9db4e0563
StevenCuasqui <cuasquisteven@gmail.com>
2018-08-07 15:44:19 -0500

    Update get_kml.py

--- get_kml.py
+++ get_kml.py
@@ -16,17 +16,13 @@ pais=sys.argv[1]
 
 #Diccionario con las vistas por coordenadas
 paises={
-    'esp':'',
-    'rus_jn30':'',
-    'cro':'',
-    'dim':'',
-    'rus_jl14':'',
-    'bel':'',
-    'eng':'',
-    'rus_jl15':'',
-    'fran':'http://localhost:5984/francia/_design/dFrancia/_view/mundial_30Jun_coord',
-    'cro':'',
-    'rus':'', 
+    'esp':'http://localhost:5984/espana/_design/dEspana/_view/coord',
+    'rus':'http://localhost:5984/rusia/_design/dRusia/_view/coord',
+    'cro':'http://localhost:5984/croacia/_design/croacia/_view/coord',
+    'dim':'http://localhost:5984/dinamarca/_design/dDin/_view/coo',
+    'bel':'http://localhost:5984/belgica/_design/dBelgica/_view/coord',
+    'eng':'http://localhost:5984/inglaterra/_design/dIngla/_view/coord',
+    'fran':'http://localhost:5984/francia/_design/dFrancia/_view/coord',
 }
 
 
